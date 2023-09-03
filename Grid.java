474e41b96a9cf17e2c868187433cc987fb35d633
jrundht <joerund.tangen@gmail.com>
2023-09-03 23:54:29 +0200

    changed whole structure

--- Grid.java
+++ Grid.java
@@ -1,46 +1,41 @@
-import java.util.ArrayList;
-
 public class Grid{
-    ArrayList<Cell> allCells = new ArrayList<>();
+    Cell[][] cells;
     int rows;
     int cols;
 
     Grid(int row, int col){
+        cells = new Cell[row*col][row*col];
         rows = row;
         cols = col;
     }
 
-    public void addCell(char value){
-        allCells.add(new Cell(value));
+    public void addCell(Cell cell, int i, int j){
+        cells[i][j] = cell;
+    }
+    
+    public void addCellArray(Cell[] oneArray, int index){
+        cells[index] = oneArray;
     }
 
-    public Cell getCell(int i){
-        return allCells.get(i);
+    public Cell getCell(int i, int j){
+        return cells[i][j];
     }
 
     @Override
-    public String toString() {
+    public String toString(){
         StringBuilder sb = new StringBuilder();
-        int gridSz = rows * rows * cols * cols;
-        
-        for(int i = 0; i < allCells.size(); i++){
-            sb.append(allCells.get(i));
-            
-            //Vertical separator
-            if((i+1) % cols == 0 && (i+1) % (rows*cols) != 0){
-                sb.append("|");
+        for(int i = 0; i < rows*cols; i++){
+            for(int j = 0; j < rows*cols; j++){
+                if(j % cols == 0 && j != 0) sb.append("|");
+                sb.append(cells[i][j]);
             }
-            if ((i+1) % (cols * rows) == 0) {
-                sb.append("\n");
-                
-                //Horizontal separators
-                if ((i + 1) % (rows * rows * cols) == 0 && (i + 1) != gridSz) {
-                    for (int j = 0; j < rows - 1; j++) {
-                        sb.append("-".repeat(cols)).append("+");
+            sb.append("\n");
+            if((i+1) % rows == 0 && i != 0){
+                for (int j = 0; j < rows - 1; j++) {
+                    sb.append("-".repeat(cols)).append("+");
                     }
-                    sb.append("-".repeat(cols));
-                    sb.append("\n");
-                }
+                sb.append("-".repeat(cols));
+                sb.append("\n");
             }
         }
         return sb.toString();
